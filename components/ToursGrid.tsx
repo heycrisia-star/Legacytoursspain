7c93bf6e56bf629c570176298d5ff0c0569add28
Cristian Gutierrez <cristiangutierrez@MacBook-Pro-de-Cristian.local>
2026-01-08 20:38:50 +0100

    Update confirmed tour structure: 1h, 2h, 3h options with new itineraries

--- components/ToursGrid.tsx
+++ components/ToursGrid.tsx
@@ -5,7 +5,7 @@ import TourCard from './TourCard';
 const ToursGrid: React.FC = () => {
   return (
     <section className="px-6 pb-4 max-w-7xl mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {TOURS.map((tour) => (
           <TourCard key={tour.id} tour={tour} />
         ))}
